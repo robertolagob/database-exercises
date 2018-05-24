6242e98251a5ce7660f392a520284c051d5dacee
robertolago <robertolagob@gmail.com>
2018-05-24 10:33:14 -0500

    Chore: Using functions for queries

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,4 +1,4 @@
-USE employees;
+UUSE employees;
 
 SELECT * FROM employees WHERE  gender='M' AND (first_name = 'Irena' OR first_name LIKE 'Vidya' OR first_name LIKE 'Maya') ORDER BY last_name,first_name  ;
 
@@ -10,11 +10,11 @@ SELECT * FROM employees WHERE birth_date  LIKE '%12-25';
 
 SELECT * FROM employees WHERE last_name  LIKE '%q%';
 
-SELECT * FROM employees WHERE  last_name  LIKE 'E%' OR last_name LIKE'%e';
-SELECT * FROM employees WHERE  last_name  LIKE 'E%e' ;
+SELECT concat(first_name,' ',last_name) AS 'Fullname' FROM employees WHERE  first_name LIKE 'E%e';
+SELECT * FROM employees WHERE  last_name  LIKE 'E%e';
 
 
 
-SELECT * FROM employees WHERE hire_date  LIKE '199%' AND birth_date  LIKE '%12-25' ORDER BY birth_date ASC ;
+SELECT concat(first_name,' ',last_name,' Days Working: ',datediff(CURDATE(),hire_date)) FROM employees WHERE hire_date  LIKE '199%' AND birth_date  LIKE '%12-25' ORDER BY birth_date ASC ;
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE'%qu%';
